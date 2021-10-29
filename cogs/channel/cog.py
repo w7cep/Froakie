c9c4f8d37cd3b1a272e7a27fe3d5ebadb802dad1
w7cep <81072374+w7cep@users.noreply.github.com>
2021-10-28 22:31:59 -0600

    update

--- cogs/channel/cog.py
+++ cogs/channel/cog.py
@@ -107,13 +107,7 @@ class Channel(commands.Cog, name="Channel"):
 	async def lockdown(self, ctx, channel: nextcord.TextChannel=None):
 		channel = channel or ctx.channel
 
-		if ctx.guild.default_role not in channel.overwrites:
-			overwrites = {
-			ctx.guild.default_role: nextcord.PermissionOverwrite(send_messages=False)
-			}
-			await channel.edit(overwrites=overwrites)
-			await ctx.send(f"I have put `{channel.name}` on lockdown.")
-		elif channel.overwrites[ctx.guild.default_role].send_messages == True or channel.overwrites[ctx.guild.default_role].send_messages == None:
+		if channel.overwrites[ctx.guild.default_role].send_messages == True or channel.overwrites[ctx.guild.default_role].send_messages == None:
 			overwrites = channel.overwrites[ctx.guild.default_role]
 			overwrites.send_messages = False
 			await channel.set_permissions(ctx.guild.default_role, overwrite=overwrites)
