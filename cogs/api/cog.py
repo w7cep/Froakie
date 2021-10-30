42de03f47c8aa1e6e577dbb5ba191eca914c5a36
w7cep <81072374+w7cep@users.noreply.github.com>
2021-10-29 22:27:34 -0600

    update

--- cogs/api/cog.py
+++ cogs/api/cog.py
@@ -3,25 +3,26 @@ from aiohttp import ClientSession
 from discord.ext import commands, tasks
 
 
-class API(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+class API(commands.Cog, name="API"):
+	"""API commands"""
+	def __init__(self, bot: commands.Bot):
+		self.bot = bot
 
-    @commands.command(name="dadjoke", aliases=['dadjokes'])
-    async def dadjoke(self, ctx):
-        url = "https://dad-jokes.p.rapidapi.com/random/jokes"
+	@commands.command(name="dadjoke", aliases=['dadjokes'])
+	async def dadjoke(self, ctx):
+		url = "https://dad-jokes.p.rapidapi.com/random/jokes"
 
-        headers = {
-            'x-rapidapi-host': "dad-jokes.p.rapidapi.com",
-            'x-rapidapi-key': self.bot.joke_api_key
-        }
+		headers = {
+			'x-rapidapi-host': "dad-jokes.p.rapidapi.com",
+			'x-rapidapi-key': self.bot.joke_api_key
+		}
 
-        async with ClientSession() as session:
-            async with session.get(url, headers=headers) as response:
-                r = await response.json()
-                r = r["body"][0]
-                await ctx.send(f"**{r['setup']}**\n\n||{r['punchline']}||")
+		async with ClientSession() as session:
+			async with session.get(url, headers=headers) as response:
+				r = await response.json()
+				r = r["body"][0]
+				await ctx.send(f"**{r['setup']}**\n\n||{r['punchline']}||")
 
 
-def setup(bot):
-    bot.add_cog(API(bot))
+def setup(bot: commands.Bot):
+		bot.add_cog(API(bot))
