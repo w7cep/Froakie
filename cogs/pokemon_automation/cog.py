2d25ac18e1ef779d10cc532989b9b1b88ff966b1
w7cep <81072374+w7cep@users.noreply.github.com>
2021-10-30 17:37:01 -0600

    update

--- cogs/pokemon_automation/cog.py
+++ cogs/pokemon_automation/cog.py
@@ -15,31 +15,41 @@ class PkmAutomation(commands.Cog, name="PkmAutomation"):
 	def __init__(self, bot: commands.Bot):
 		self.bot = bot
 	
-	@commands.command(name="arduino_release")
+	@commands.group(invoke_without_command=True)
 	@commands.guild_only()
-	async def arduino_release(self, ctx):
+	async def arduino(self, ctx):
+		await ctx.send("Invalid sub-command passed")
+ 
+	@arduino.command(name="release")
+	@commands.guild_only()
+	async def release(self, ctx):
 		await ctx.send("https://github.com/PokemonAutomation/SwSh-Arduino/releases")
 	
-	@commands.command(name="arduino_wiki")
+	@arduino.command(name="wiki")
 	@commands.guild_only()
-	async def arduino_wiki(self, ctx):
+	async def wiki(self, ctx):
 		await ctx.send("https://github.com/PokemonAutomation/SwSh-Arduino/wiki")
- 
-	@commands.command(name="forkbot_release")
-	@commands.guild_only()
-	async def forkbot_release(self, ctx):
-		await ctx.send("https://dev.azure.com/koigithub3088/SysBot_KoiFork/_build?definitionId=2&_a=summary")
-	
-	@commands.command(name="forkbot_wiki")
-	@commands.guild_only()
-	async def forkbot_wiki(self, ctx):
-		await ctx.send("https://github.com/Koi-3088/ForkBot.NET/wiki")
 
-	@commands.command(name="arduino_beta")
+	@arduino.command(name="beta")
 	@commands.guild_only()
-	async def arduino_beta(self, ctx):
+	async def beta(self, ctx):
 		await ctx.channel.trigger_typing()
 		await ctx.send("https://github.com/w7cep/Arduino-Beta")
   
+	@commands.group(invoke_without_command=True)
+	@commands.guild_only()
+	async def forkbot(self, ctx):
+		await ctx.send("Invalid sub-command passed")
+  
+	@forkbot.command(name="release")
+	@commands.guild_only()
+	async def _release(self, ctx):
+		await ctx.send("https://dev.azure.com/koigithub3088/SysBot_KoiFork/_build?definitionId=2&_a=summary")
+	
+	@forkbot.command(name="wiki")
+	@commands.guild_only()
+	async def _wiki(self, ctx):
+		await ctx.send("https://github.com/Koi-3088/ForkBot.NET/wiki")
+
 def setup(bot: commands.Bot):
 	bot.add_cog(PkmAutomation(bot))
