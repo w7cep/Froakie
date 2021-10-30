ce35a60eb6c928f755c8c79d89a36f034d138532
w7cep <81072374+w7cep@users.noreply.github.com>
2021-10-30 00:40:47 -0600

    update

--- cogs/member_mod/cog.py
+++ cogs/member_mod/cog.py
@@ -66,7 +66,7 @@ class Mod(commands.Cog, name="Mod"):
 		await ctx.guild.ban(user)
 
 		ban= nextcord.Embed(title=f'A moderation action has been performed!', description='', color=0x90fd05)
-		ban.add_field(name='User Affected:', value=f'`{user.name}``{user.discriminator}`', inline=True)
+		ban.add_field(name='User Affected:', value=f'`{user.name}{user.discriminator}`', inline=True)
 		ban.add_field(name='User ID:', value=f'`{user.id}`', inline=True)
 		ban.add_field(name='Moderator Name:', value=f'`{ctx.author}`', inline=True)
 		ban.add_field(name='Moderator ID:', value=f'`{ctx.author.id}`', inline=True)
@@ -85,7 +85,7 @@ class Mod(commands.Cog, name="Mod"):
 		user = await self.bot.fetch_user(id)
 
 		softban= nextcord.Embed(title=f'A moderation action has been performed!', description='', color=0x90fd05)
-		softban.add_field(name='User Affected:', value=f'`{user.name}``{user.discriminator}`', inline=True)
+		softban.add_field(name='User Affected:', value=f'`{user.name}{user.discriminator}`', inline=True)
 		softban.add_field(name='User ID:', value=f'`{user.id}`', inline=True)
 		softban.add_field(name='Moderator Name:', value=f'`{ctx.author}`', inline=True)
 		softban.add_field(name='Moderator ID:', value=f'`{ctx.author.id}`', inline=True)
@@ -106,9 +106,9 @@ class Mod(commands.Cog, name="Mod"):
 		await ctx.guild.unban(user)
 
 		unban= nextcord.Embed(title=f'A moderation action has been performed!', description='', color=0x90fd05)
-		unban.add_field(name='User Affected:', value=f'`{user.name}``{user.discriminator}`', inline=True)
+		unban.add_field(name='User Affected:', value=f'`{user.name}{user.discriminator}`', inline=True)
 		unban.add_field(name='User ID:', value=f'`{user.id}`', inline=True)
-		unban.add_field(name='Moderator Name:', value=f'`{ctx.author}`', inline=True)
+		unban.add_field(name='Moderator Name:', value=f'`{ctx.author}`', inline=False)
 		unban.add_field(name='Moderator ID:', value=f'`{ctx.author.id}`', inline=True)
 		unban.add_field(name='Action Performed:', value='`Unban`', inline=True)
 		unban.set_author(name=f'{ctx.guild}', icon_url=ctx.guild.icon.url)
@@ -126,7 +126,7 @@ class Mod(commands.Cog, name="Mod"):
 		await ctx.guild.kick(user)
 
 		kick= nextcord.Embed(title=f'A moderation action has been performed!', description='', color=0x90fd05)
-		kick.add_field(name='User Affected:', value=f'`{user.name}``{user.discriminator}`', inline=True)
+		kick.add_field(name='User Affected:', value=f'`{user.name}{user.discriminator}`', inline=True)
 		kick.add_field(name='User ID:', value=f'`{user.id}`', inline=True)
 		kick.add_field(name='Moderator Name:', value=f'`{ctx.author}`', inline=True)
 		kick.add_field(name='Moderator ID:', value=f'`{ctx.author.id}`', inline=True)
