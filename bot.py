b2bfcc40f695522b87e6b086f1d9a0c7508b0acd
w7cep <81072374+w7cep@users.noreply.github.com>
2021-10-31 14:14:11 -0600

    update

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ def main():
 			bot.load_extension(f"cogs.{folder}.cog")
 
 
-	@tasks.loop(minute=10)  # you can even use hours and minutes
+	@tasks.loop(minutes=10)  # you can even use hours and minutes
 	async def send_message():
 		await bot.get_channel(904434133793656932).send("**Reminder**\n\nUsing the bot is fun, to keep it fun for everyone, please complete your trade with the bot. Even when making a mistake and you've started the trade, complete it.\nSee it as a free item and fodder you don't have to catch.\n\nPlease don't delete messages. Even when it's a mistake.\nMakes trouble-shooting difficult.\n\nBot access will be revoked for multiple offenders.")
 	
