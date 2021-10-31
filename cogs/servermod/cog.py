b7591d494eb73390ec6fc3f59918347f6b43ce53
w7cep <81072374+w7cep@users.noreply.github.com>
2021-10-31 14:38:51 -0600

    update

--- cogs/servermod/cog.py
+++ cogs/servermod/cog.py
@@ -13,7 +13,7 @@ class ServerMod(commands.Cog, name="ServerMod"):
 		self.send_message.start()
 
 	@tasks.loop(minutes=10)  # you can even use hours and minutes
-	async def send_message(self, ctx):
+	async def send_message(self):
 		print("Sending message")
 		await self.bot.get_channel(904434133793656932).send("**Reminder**\n\nUsing the bot is fun, to keep it fun for everyone, please complete your trade with the bot. Even when making a mistake and you've started the trade, complete it.\nSee it as a free item and fodder you don't have to catch.\n\nPlease don't delete messages. Even when it's a mistake.\nMakes trouble-shooting difficult.\n\nBot access will be revoked for multiple offenders.")
 
