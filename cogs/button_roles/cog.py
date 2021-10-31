444d06a266f61d33d7b99114dad843653092c2b2
w7cep <81072374+w7cep@users.noreply.github.com>
2021-10-31 13:57:16 -0600

    update

--- cogs/button_roles/cog.py
+++ cogs/button_roles/cog.py
@@ -73,7 +73,7 @@ class ButtonRolesCog(commands.Cog, name="Roles"):
 	   	)
 		embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/891852099653083186/895902400416710666/greninja-frogadier.gif")
 		embed.set_image(url="https://cdn.discordapp.com/attachments/859634488593743892/891612213654192168/greninja_banner.jpg")
-
+		embed.set_author(name="Greninja Mod", icon_url="https://cdn.discordapp.com/avatars/892620195342987274/cb32b40409c7df4d147c400582f939ac.webp?size=128")
 		embed.set_footer(text="Bot is running v1.0.0")
 		await ctx.channel.trigger_typing()
 		await ctx.send(embed=embed, view=ConfirmView())
@@ -91,7 +91,7 @@ class ButtonRolesCog(commands.Cog, name="Roles"):
 			colour=nextcord.Colour.blue()
 		)
 		embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/891852099653083186/895902400416710666/greninja-frogadier.gif")
-		embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar.url)
+		embed.set_author(name="Greninja Mod", icon_url="https://cdn.discordapp.com/avatars/892620195342987274/cb32b40409c7df4d147c400582f939ac.webp?size=128")
 		embed.set_image(url="https://cdn.discordapp.com/attachments/859634488593743892/891612213654192168/greninja_banner.jpg")
 		embed.set_footer(text=f"Bot is running {version}")
 		await ctx.channel.trigger_typing()
