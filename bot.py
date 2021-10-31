763d23bdf7a47a63ff6617eb3a3e851bf6ea7258
w7cep <81072374+w7cep@users.noreply.github.com>
2021-10-31 12:27:15 -0600

    update

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 import os
+
 import aiohttp
 import nextcord
 import nextcord.ext
 from nextcord.ext import commands, tasks
+
 import config
+
 # TODO: Fine tune command permissions.
 # TODO: Clean up import modules on every file.
 # TODO: Delete every excess space / Convert files with space indents to tabs.
@@ -22,21 +25,21 @@ def main():
 	@bot.event
 	async def on_ready():
 		print(f"{bot.user.name} has connected to Discord.")
-
 	# load all cogs
 	for folder in os.listdir("cogs"):
 		if os.path.exists(os.path.join("cogs", folder, "cog.py")):
 			bot.load_extension(f"cogs.{folder}.cog")
 
-	'''	@tasks.loop(hours=6)  # you can even use hours and minutes
+
+	@tasks.loop(hours=8)  # you can even use hours and minutes
 	async def send_message():
-		await bot.get_channel(843271842931933224).send("**Reminder**\n\nUsing the bot is fun, to keep it fun for everyone, please complete your trade with the bot. Even when making a mistake and you've started the trade, complete it.\nSee it as a free item and fodder you don't have to catch.\n\nPlease don't delete messages. Even when it's a mistake.\nMakes trouble-shooting difficult.\n\nBot access will be revoked for multiple offenders.")'''
+		await bot.get_channel(904434133793656932).send("**Reminder**\n\nUsing the bot is fun, to keep it fun for everyone, please complete your trade with the bot. Even when making a mistake and you've started the trade, complete it.\nSee it as a free item and fodder you don't have to catch.\n\nPlease don't delete messages. Even when it's a mistake.\nMakes trouble-shooting difficult.\n\nBot access will be revoked for multiple offenders.")
 	
 	async def startup():
 		bot.session = aiohttp.ClientSession()
 
 	bot.loop.create_task(startup())
-	
+ 	send_message.start()
 	# run the bot
 	bot.run(config.BOT_TOKEN)
 
