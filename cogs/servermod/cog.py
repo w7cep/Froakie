08574103d048399d8a642975c91d7175afaffa5f
w7cep <81072374+w7cep@users.noreply.github.com>
2021-10-31 21:46:40 -0600

    update

--- cogs/servermod/cog.py
+++ cogs/servermod/cog.py
@@ -10,15 +10,16 @@ class ServerMod(commands.Cog, name="ServerMod"):
 	def __init__(self, bot: commands.Bot):
 		self.bot = bot
 		self._last_member = None
-		self.send_message.start()
+		self.bot_reminder.start()
 
 	@tasks.loop(hours=8)  # you can even use hours and minutes
-	async def send_message(self):
+	async def bot_reminder(self):
 		print("Sending message")
-		await self.bot.get_channel(904434133793656932).send("**Reminder**\n\nUsing the bot is fun, to keep it fun for everyone, please complete your trade with the bot. Even when making a mistake and you've started the trade, complete it.\nSee it as a free item and fodder you don't have to catch.\n\nPlease don't delete messages. Even when it's a mistake.\nMakes trouble-shooting difficult.\n\nBot access will be revoked for multiple offenders.")
+		channel = self.bot.get_channel(843271842931933224)
+		await channel.send("**Reminder**\n\nUsing the bot is fun, to keep it fun for everyone, please complete your trade with the bot. Even when making a mistake and you've started the trade, complete it.\nSee it as a free item and fodder you don't have to catch.\n\nPlease don't delete messages. Even when it's a mistake.\nMakes trouble-shooting difficult.\n\nBot access will be revoked for multiple offenders.")
 
-	@send_message.before_loop
-	async def before_printer(self):
+	@bot_reminder.before_loop
+	async def before_bot_reminder(self):
 		print('waiting...')
 		await self.bot.wait_until_ready()
 
